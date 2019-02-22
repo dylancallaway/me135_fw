099e863db6754d1c60ecfc72b0aeb03ea204b1aa
dylan <dylancallaway@berkeley.edu>
2019-02-22 14:48:29 -0800

    getting ready

--- main.cpp
+++ main.cpp
@@ -1,33 +1,14 @@
+/**
+ * Firmware for Bair Hockey image processing and motor control
+ * Written using MBED OS 5 running on NUCLEO-F767Z1 Arm Cortex M7 microcontroller
+**/
+
 #include "mbed.h"
 
-void print_char(char c = '$')
-{
-    printf("%c", c);
-    fflush(stdout);
-}
+// STM32F7 DCMI driver
+#include "stm32f7xx_hal_dcmi.h"
 
-Thread thread;
-
-DigitalOut led1(LED1);
-
-void print_thread()
-{
-    while (true)
-    {
-        wait(1);
-        print_char();
-    }
-}
 
 int main()
 {
-    printf("\n\n*** RTOS basic example ***\n");
-
-    thread.start(print_thread);
-
-    while (true)
-    {
-        led1 = !led1;
-        wait(0.5);
-    }
 }
