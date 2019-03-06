2ce5ef71ccc0a66ed4c3a57772842255c0cc6d82
dylan <dylancallaway@berkeley.edu>
2019-03-05 22:48:50 -0800

    first camera communication

--- main.cpp
+++ main.cpp
@@ -50,16 +50,20 @@ DigitalOut led1(LED1); // Green <- Good things LED
 DigitalOut led2(LED2); // Blue <- Debug LED
 DigitalOut led3(LED3); // Red <- Error LED
 
+// Config USER button
+DigitalIn usr_but(USER_BUTTON); // TODO attach interrupt to the button to run i2c_scan when pressed and light LED if a device is found.
+
 // Camera config pins
-DigitalOut camera_RST(PA_5);
-DigitalOut camera_PWDN(PD_14);
+DigitalOut camera_RST(PA_5);   // Reset (active low) (must be high for camera to operate)
+DigitalOut camera_PWDN(PD_14); // Power down (active high) (must be low for camera to operate)
 
 // I2C *** NEED EXTERNAL PULLUPS ON THESE PINS ***
-I2C i2c(PB_11, PB_10); // I2C2_SCL = PB_10, I2C2_SDA = PB_11
-// I2C i2c(I2C_SDA, I2C_SCL); // I2C2_SCL = PB_10, I2C2_SDA = PB_11
+// OV2640 7 bit address: 0x30
+I2C i2c(PB_11, PB_10); // I2C2_SDA = PB_11, I2C2_SCL = PB_10
+// I2C i2c(I2C_SDA, I2C_SCL);
 
-// Serial communication
-// Serial pc(SERIAL_TX, SERIAL_RX, 115200);
+// Serial communication at 115200 baud
+Serial pc(SERIAL_TX, SERIAL_RX, 115200);
 
 void GPIO_Config(void)
 {
@@ -80,9 +84,6 @@ void GPIO_Config(void)
 
     // This initializes pins X4 and X6 where X is any letter A-Z
    */
-
-    // TODO Figure out what pins to set as GPIO for DCMI
-    // Is it arbitrary or what?
 
     /**DCMI GPIO Configuration
      * https://www.st.com/resource/en/datasheet/stm32f767zi.pdf -> Pinouts and pin description
@@ -145,6 +146,9 @@ void GPIO_Config(void)
     gpio_init_structure.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
     gpio_init_structure.Alternate = GPIO_AF13_DCMI;
     HAL_GPIO_Init(GPIOG, &gpio_init_structure);
+
+    camera_RST = 1;
+    camera_PWDN = 0;
 }
 
 void DMA_Config(void)
@@ -208,20 +212,129 @@ void DCMI_Config(void)
     dcmi_init_structure.Instance = DCMI;
 
     led3 = HAL_DCMI_Init(&dcmi_init_structure); // Initialize DCMI with given parameter structure
-
-    /* Enable the DMA ------------------ MAYBE NEEDED ??? */
-    // __HAL_DMA_ENABLE(dcmi_init_structure.DMA_Handle);
 }
 
-void camera_hw_reset(void)
+void i2c_scan(void)
 {
-    camera_RST = 0;  // Active low
-    camera_PWDN = 1; // Active high
-    HAL_Delay(100);
-    camera_PWDN = 0;
-    HAL_Delay(3);
-    camera_RST = 1;
-    HAL_Delay(6);
+    int error, address;
+    int nDevices;
+
+    printf("Scanning I2C bus...\n");
+
+    nDevices = 0;
+
+    char write_data[1];
+    write_data[0] = 0x00;
+
+    for (address = 1; address < 127; address++)
+    {
+        i2c.start();
+        error = i2c.write(address << 1, write_data, 1, 0); //We shift it left because mbed takes in 8 bit addreses
+        i2c.stop();
+        if (error == 0)
+        {
+            printf("I2C device found at address 0x%X.", address); //Returns 7-bit addres
+            nDevices++;
+        }
+    }
+    printf("\nScan complete.\n");
+
+    if (nDevices == 0)
+    {
+        printf("No I2C devices found.\n");
+        led2 = 0;
+    }
+    else
+    {
+        led2 = 1;
+        HAL_Delay(500);
+    }
+}
+
+/**
+  * @brief  System Clock Configuration
+  *         The system Clock is configured as follow : 
+  *            System Clock source            = PLL (HSE)
+  *            SYSCLK(Hz)                     = 200000000
+  *            HCLK(Hz)                       = 200000000
+  *            AHB Prescaler                  = 1
+  *            APB1 Prescaler                 = 4
+  *            APB2 Prescaler                 = 2
+  *            HSE Frequency(Hz)              = 25000000
+  *            PLL_M                          = 25
+  *            PLL_N                          = 400
+  *            PLL_P                          = 2
+  *            PLL_Q                          = 8
+  *            PLL_R                          = 7
+  *            VDD(V)                         = 3.3
+  *            Main regulator output voltage  = Scale1 mode
+  *            Flash Latency(WS)              = 6
+  * @param  None
+  * @retval None
+  */
+static void SystemClock_Config(void)
+{
+    RCC_OscInitTypeDef RCC_OscInitStruct;
+    RCC_ClkInitTypeDef RCC_ClkInitStruct;
+    HAL_StatusTypeDef ret = HAL_OK;
+
+    /* Enable Power Control clock */
+    __HAL_RCC_PWR_CLK_ENABLE();
+
+    /* The voltage scaling allows optimizing the power consumption when the device is 
+     clocked below the maximum system frequency, to update the voltage scaling value 
+     regarding system frequency refer to product datasheet.  */
+    __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+    /* Enable HSE Oscillator and activate PLL with HSE as source */
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+    RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE; // Was originally RCC_PLL_ON, but ON or OFF cause error
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+    RCC_OscInitStruct.PLL.PLLM = 25;
+    RCC_OscInitStruct.PLL.PLLN = 400;
+    RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+    RCC_OscInitStruct.PLL.PLLQ = 8;
+    RCC_OscInitStruct.PLL.PLLR = 7;
+
+    ret = HAL_RCC_OscConfig(&RCC_OscInitStruct);
+    if (ret != HAL_OK)
+    {
+        while (1)
+        {
+            led3 = !led3;
+            HAL_Delay(250);
+        }
+    }
+
+    /* Activate the OverDrive to reach the 200 MHz Frequency */
+    ret = HAL_PWREx_EnableOverDrive();
+    if (ret != HAL_OK)
+    {
+        while (1)
+        {
+            led3 = !led3;
+            HAL_Delay(250);
+        }
+    }
+
+    /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2 clocks dividers */
+    RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+
+    ret = HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_6);
+    if (ret != HAL_OK)
+    {
+        while (1)
+        {
+            led3 = !led3;
+            HAL_Delay(250);
+            ;
+        }
+    }
 }
 
 void led1_heartbeat(void)
@@ -229,35 +342,25 @@ void led1_heartbeat(void)
     led1 = !led1;
 }
 
-int main(void)
+int main()
 {
-    // camera_hw_reset();
+    SystemClock_Config(); // Not entirely sure exactly what is needed here but this seems to work.
+
+    // Output clock on MCO1 -> PA_8 (for OV2640 XCLK)
+    // This is needed for any communication with the camera, SCCB included
+    // TODO check frequency with scope
+    HAL_RCC_MCOConfig(RCC_MCO1, RCC_MCO1SOURCE_HSE, RCC_MCODIV_1);
+
+    i2c.frequency(400000); // Can only use 100kHz and 400kHz for SCCB
+
     GPIO_Config();
+
     DMA_Config();
+
     DCMI_Config();
-    led3 = HAL_DCMI_Start_DMA(&dcmi_init_structure, DCMI_MODE_CONTINUOUS, DCMI_MEMORY_LOC, DCMI_MEMORY_LEN); // Enable DCMI, DMA and capture a frame
 
-    // RCC_ClkInitTypeDef RCC_ClkInitStruct;
-    // RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK;
-    // RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSE;
-    // RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-    // led2 = HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_8); // FLatecy of 8, 9, or 10 ?? WHY ??
-
-    // // Output clock on MCO1 -> PA_8
-    // HAL_RCC_MCOConfig(RCC_MCO1, RCC_MCO1SOURCE_PLLCLK, RCC_MCODIV_1);
-
-    // char recv[1];
-    // dev = 0x61;
-
-    // char cmd[1];
-    // cmd[0] = 0xFF;
-
-    // led2 = i2c.write(0x60 << 1, cmd, 8);
-    // led3 = i2c.write(ox61 << 1, 0x0A, 4);
-    // led2 = i2c.write(0x61 << 1, 0x61, 7);
-    // led3 = i2c.read(0x61 << 1, recv, 8);
-
-    // pc.printf("\n%p", recv);
+    // Enable DCMI, DMA and capture a frame
+    led3 = HAL_DCMI_Start_DMA(&dcmi_init_structure, DCMI_MODE_CONTINUOUS, DCMI_MEMORY_LOC, DCMI_MEMORY_LEN);
 
     // Turn on heartbeat
     heartbeat_ticker.attach(led1_heartbeat, 0.5);
