22d8daaace9049ea17ba2f0135119114ad039ced
dylan <dylancallaway@berkeley.edu>
2019-03-17 21:34:12 -0700

    fast push

--- compile.sh
+++ compile.sh
@@ -1,1 +1,1 @@
-g++ main.cpp -o test -linclude -lwiringPi -lrt -lpthread -Wall `pkg-config --cflags --libs opencv4.pc`
+g++ main.cpp RTLib.cpp -o test -Iinclude -lwiringPi -lrt -lpthread -Wall `pkg-config --cflags --libs opencv4.pc`
