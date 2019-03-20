e0b80f55b6f39e3eb82a21704cd733ce66a7cb75
dylan <dylancallaway@berkeley.edu>
2019-03-19 18:46:01 -0700

    fast push

--- main.cpp
+++ main.cpp
@@ -9,19 +9,11 @@ using namespace cv;
 #include <sched.h>
 #include <sys/mman.h>
 
-#define FRM_WIDTH_COLS 640
-#define FRM_HEIGHT_ROWS 480
+#define FRM_COLS 640
+#define FRM_ROWS 480
 
-int main(int argc, char **argv)
+VideoCapture init_camera(void)
 {
-    Mat src(FRM_HEIGHT_ROWS, FRM_WIDTH_COLS, CV_8UC3, Scalar(0, 0, 0));
-    Mat b(FRM_HEIGHT_ROWS, FRM_WIDTH_COLS, CV_8UC1, Scalar(0));
-    Mat g(FRM_HEIGHT_ROWS, FRM_WIDTH_COLS, CV_8UC1, Scalar(0));
-    Mat r(FRM_HEIGHT_ROWS, FRM_WIDTH_COLS, CV_8UC1, Scalar(0));
-    Mat bgr[3] = {b, g, r};
-
-    wiringPiSetup();
-
     VideoCapture cam(0);
 
     if (!cam.isOpened())
@@ -31,8 +23,8 @@ int main(int argc, char **argv)
         return -1;
     }
 
-    double frm_width = FRM_WIDTH_COLS;
-    double frm_height = FRM_HEIGHT_ROWS;
+    double frm_width = FRM_COLS;
+    double frm_height = FRM_ROWS;
     cam.set(CAP_PROP_FRAME_WIDTH, frm_width);
     cam.set(CAP_PROP_FRAME_HEIGHT, frm_height);
 
@@ -42,8 +34,23 @@ int main(int argc, char **argv)
     cam.set(CAP_PROP_FPS, frm_rate);
     cout << "Frame rate: " << frm_rate << "\n";
 
+    return cam;
+}
+
+int main(int argc, char **argv)
+{
+    wiringPiSetup();
+
+    cam = init_camera();
+
     // string window_name = "Camera Feed";
     // namedWindow(window_name, WINDOW_NORMAL);
+
+    Mat src(FRM_ROWS, FRM_COLS, CV_8UC3, Scalar(0, 0, 0));
+    Mat b(FRM_ROWS, FRM_COLS, CV_8UC1, Scalar(0));
+    Mat g(FRM_ROWS, FRM_COLS, CV_8UC1, Scalar(0));
+    Mat r(FRM_ROWS, FRM_COLS, CV_8UC1, Scalar(0));
+    Mat bgr[3] = {b, g, r};
 
     clock_t t1, t2, delta_t;
 
