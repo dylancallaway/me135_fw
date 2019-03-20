a337576c22487632b966cf27a06b584734031c39
root <dylancallaway@berkeley.edu>
2019-03-20 11:30:39 -0700

    fast push

--- include/RTLib.cpp
+++ include/RTLib.cpp
@@ -1,14 +1,5 @@
 #include <RTLib.h>
 
-void setprio(int prio, int sched)
-{
-    struct sched_param param;
-    // Set realtime priority for this thread
-    param.sched_priority = prio;
-    if (sched_setscheduler(0, sched, &param) < 0)
-        perror("sched_setscheduler");
-}
-
 void show_new_pagefault_count(const char *logtext, const char *allowed_maj, const char *allowed_min)
 {
     int last_majflt = 0, last_minflt = 0;
@@ -24,71 +15,6 @@ void show_new_pagefault_count(const char *logtext, const char *allowed_maj, cons
 
     last_majflt = usage.ru_majflt;
     last_minflt = usage.ru_minflt;
-}
-
-void prove_thread_stack_use_is_safe(int stacksize)
-{
-    volatile char buffer[stacksize];
-    int i;
-
-    /* Prove that this thread is behaving well */
-    for (i = 0; i < stacksize; i += sysconf(_SC_PAGESIZE))
-    {
-        /* Each write to this buffer shall NOT generate a pagefault. */
-        buffer[i] = i;
-    }
-
-    show_new_pagefault_count("Caused by using thread stack", "0", "0");
-}
-
-/*************************************************************/
-/* The thread to start */
-void *my_rt_thread(void *args)
-{
-    struct timespec ts;
-    ts.tv_sec = 30;
-    ts.tv_nsec = 0;
-
-    setprio(sched_get_priority_max(SCHED_RR), SCHED_RR);
-
-    printf("I am an RT-thread with a stack that does not generate "
-           "page-faults during use, stacksize=%i\n",
-           MY_STACK_SIZE);
-
-    //<do your RT-thing here>
-
-    show_new_pagefault_count("Caused by creating thread", ">=0", ">=0");
-
-    prove_thread_stack_use_is_safe(MY_STACK_SIZE);
-
-    /* wait 30 seconds before thread terminates */
-    clock_nanosleep(CLOCK_REALTIME, 0, &ts, NULL);
-
-    return NULL;
-}
-
-/*************************************************************/
-
-void error(int at)
-{
-    /* Just exit on error */
-    fprintf(stderr, "Some error occured at %d", at);
-    exit(1);
-}
-
-void start_rt_thread(void)
-{
-    pthread_t thread;
-    pthread_attr_t attr;
-
-    /* init to default values */
-    if (pthread_attr_init(&attr))
-        error(1);
-    /* Set the requested stacksize for this thread */
-    if (pthread_attr_setstacksize(&attr, PTHREAD_STACK_MIN + MY_STACK_SIZE))
-        error(2);
-    /* And finally start the actual thread */
-    pthread_create(&thread, &attr, my_rt_thread, NULL);
 }
 
 void configure_malloc_behavior(void)
