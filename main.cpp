90bd98cce6e3ce10e184829ea490417ba196686a
dylan <dylancallaway@berkeley.edu>
2019-03-19 21:20:29 -0700

    fast push

--- main.cpp
+++ main.cpp
@@ -5,23 +5,52 @@ using namespace std;
 using namespace cv;
 
 #include <wiringPi.h>
+#include <thread>
 
-#include <sched.h>
-#include <sys/mman.h>
+#define PRE_ALLOCATION_SIZE (100 * 1024 * 1024) /* 100MB - total amount of memory locked for this process */
+#define MY_STACK_SIZE (10 * 1024 * 1024)        /* 10MB - stack size for this process */
+
+#include <RTLib.h>
 
 #define FRM_COLS 640
 #define FRM_ROWS 480
 
-VideoCapture init_camera(void)
-{
-    VideoCapture cam(0);
+Mat src(FRM_ROWS, FRM_COLS, CV_8UC3, Scalar(0, 0, 0));
+Mat b(FRM_ROWS, FRM_COLS, CV_8UC1, Scalar(0));
+Mat g(FRM_ROWS, FRM_COLS, CV_8UC1, Scalar(0));
+Mat r(FRM_ROWS, FRM_COLS, CV_8UC1, Scalar(0));
+Mat bgr[3] = {b, g, r};
 
-    if (!cam.isOpened())
-    {
-        cout << "Unable to open camera.\n";
-        cin.get();
-        return -1;
-    }
+VideoCapture cam(0);
+
+int t1, t2, t_elapse;
+
+int main(int argc, char **argv)
+{
+    show_new_pagefault_count("Initial count", ">=0", ">=0");
+
+    configure_malloc_behavior();
+
+    show_new_pagefault_count("mlockall() generated", ">=0", ">=0");
+
+    reserve_process_memory(PRE_ALLOCATION_SIZE);
+
+    show_new_pagefault_count("malloc() and touch generated",
+                             ">=0", ">=0");
+
+    /* Now allocate the memory for the 2nd time and prove the number of
+	   pagefaults are zero */
+    reserve_process_memory(PRE_ALLOCATION_SIZE);
+    show_new_pagefault_count("2nd malloc() and use generated",
+                             "0", "0");
+
+    printf("\n\nLook at the output of ps -leyf, and see that the "
+           "RSS is now about %d [MB]\n",
+           PRE_ALLOCATION_SIZE / (1024 * 1024));
+
+    start_rt_thread();
+
+    wiringPiSetup();
 
     double frm_width = FRM_COLS;
     double frm_height = FRM_ROWS;
@@ -34,54 +63,24 @@ VideoCapture init_camera(void)
     cam.set(CAP_PROP_FPS, frm_rate);
     cout << "Frame rate: " << frm_rate << "\n";
 
-    return cam;
-}
-
-int main(int argc, char **argv)
-{
-    wiringPiSetup();
-
-    cam = init_camera();
-
     // string window_name = "Camera Feed";
     // namedWindow(window_name, WINDOW_NORMAL);
 
-    Mat src(FRM_ROWS, FRM_COLS, CV_8UC3, Scalar(0, 0, 0));
-    Mat b(FRM_ROWS, FRM_COLS, CV_8UC1, Scalar(0));
-    Mat g(FRM_ROWS, FRM_COLS, CV_8UC1, Scalar(0));
-    Mat r(FRM_ROWS, FRM_COLS, CV_8UC1, Scalar(0));
-    Mat bgr[3] = {b, g, r};
-
-    clock_t t1, t2, delta_t;
-
     while (true)
     {
-        t1 = clock();
-        delta_t = t1 - t2;
-        t2 = clock();
-
-        bool read_success = cam.read(src);
-
-        if (!read_success)
-        {
-            cout << "Unable to read frame.\n";
-            cin.get(); // Wait for keypress
-            break;
-        }
-
+        // t1 = micros();
+        cam.read(src);
         split(src, bgr);
 
         // imshow(window_name, src);
-
         // if (waitKey(10) == 27)
         // {
         // cout << "Esc key pressed, stopping feed.\n";
         // break;
         // }
 
-        cout << delta_t << "\n"; // in clock ticks
-
-        // delayMicroseconds(10); // Delay allows background processes to run at a fixed interval
+        // t2 = micros();
+        // cout << (t2 - t1) << "\n";
     }
 
     return 0;
