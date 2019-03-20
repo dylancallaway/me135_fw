dfc40976e361eb799e9acb813168c9f9a4b6456b
root <dylancallaway@berkeley.edu>
2019-03-20 11:48:39 -0700

    fast push

---
+++ legacy/include/RTLib.cpp
@@ -0,0 +1,2 @@
+#include <RTLib.h>
+
--- main.cpp
+++ main.cpp
@@ -12,22 +12,23 @@ using namespace cv;
 #include <chrono>
 #include <sys/mman.h>
 
-#include <RTLib.h>
-
-#define PRE_ALLOCATION_SIZE (100 * 1024 * 1024) /* 100MB pagefault free buffer */
-
 #define FRM_COLS 640
 #define FRM_ROWS 480
 #define FRM_RATE 90
 #define CAP_INTERVAL 12
 
-Mat src(FRM_ROWS, FRM_COLS, CV_8UC3, Scalar(0, 0, 0));
-Mat b(FRM_ROWS, FRM_COLS, CV_8UC1, Scalar(0));
-Mat g(FRM_ROWS, FRM_COLS, CV_8UC1, Scalar(0));
-Mat r(FRM_ROWS, FRM_COLS, CV_8UC1, Scalar(0));
-Mat bgr[3] = {b, g, r};
+#define PRE_ALLOCATION_SIZE 100 * 1000 * 1000 /*Size of pagefault free buffer in bytes */
+void show_new_pagefault_count(const char *logtext, const char *allowed_maj, const char *allowed_min);
+void configure_malloc_behavior(void);
+void reserve_process_memory(int size);
 
-VideoCapture cam(0);
+// Mat src(FRM_ROWS, FRM_COLS, CV_8UC3, Scalar(0, 0, 0));
+// Mat b(FRM_ROWS, FRM_COLS, CV_8UC1, Scalar(0));
+// Mat g(FRM_ROWS, FRM_COLS, CV_8UC1, Scalar(0));
+// Mat r(FRM_ROWS, FRM_COLS, CV_8UC1, Scalar(0));
+// Mat bgr[3] = {b, g, r};
+
+// VideoCapture cam(0);
 
 int main()
 {
@@ -53,10 +54,10 @@ int main()
                              "0", "0");
 
     printf("\n\nLook at the output of ps -leyf, and see that the "
-           "RSS is now about %d [MB]\n",
-           PRE_ALLOCATION_SIZE / (1024 * 1024));
+           "RSS is now about %d [kB]\n",
+           PRE_ALLOCATION_SIZE / (1000));
 
-    wiringPiSetup();
+    // wiringPiSetup();
 
     // cam.set(CAP_PROP_FRAME_WIDTH, FRM_COLS);
     // cam.set(CAP_PROP_FRAME_HEIGHT, FRM_ROWS);
@@ -94,5 +95,66 @@ int main()
     //     this_thread::sleep_until(next_time);
     // }
 
+    while (true)
+    {
+        delay(10);
+    }
+
     return 0;
+}
+
+void show_new_pagefault_count(const char *logtext, const char *allowed_maj, const char *allowed_min)
+{
+    int last_majflt = 0, last_minflt = 0;
+    struct rusage usage;
+
+    getrusage(RUSAGE_SELF, &usage);
+
+    printf("%-30.30s: Pagefaults, Major:%ld (Allowed %s), "
+           "Minor:%ld (Allowed %s)\n",
+           logtext,
+           usage.ru_majflt - last_majflt, allowed_maj,
+           usage.ru_minflt - last_minflt, allowed_min);
+
+    last_majflt = usage.ru_majflt;
+    last_minflt = usage.ru_minflt;
+}
+
+void configure_malloc_behavior(void)
+{
+    /* Now lock all current and future pages 
+	   from preventing of being paged */
+    if (mlockall(MCL_CURRENT | MCL_FUTURE))
+        perror("mlockall failed:");
+
+    /* Turn off malloc trimming.*/
+    mallopt(M_TRIM_THRESHOLD, -1);
+
+    /* Turn off mmap usage. */
+    mallopt(M_MMAP_MAX, 0);
+}
+
+void reserve_process_memory(int size)
+{
+    int i;
+    char *buffer;
+    buffer = (char *)malloc(size);
+
+    /* Touch each page in this piece of memory to get it mapped into RAM */
+    for (i = 0; i < size; i += sysconf(_SC_PAGESIZE))
+    {
+        /* Each write to this buffer will generate a pagefault.
+		   Once the pagefault is handled a page will be locked in
+		   memory and never given back to the system. */
+        buffer[i] = 0;
+    }
+
+    /* buffer will now be released. As Glibc is configured such that it 
+	   never gives back memory to the kernel, the memory allocated above is
+	   locked for this process. All malloc() and new() calls come from
+	   the memory pool reserved and locked above. Issuing free() and
+	   delete() does NOT make this locking undone. So, with this locking
+	   mechanism we can build C++ applications that will never run into
+	   a major/minor pagefault, even with swapping enabled. */
+    free(buffer);
 }
